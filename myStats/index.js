9d811e52fd3357e6613ff6193d7a6fd9cd398c29
SimonLeclere <simon-leclere@orange.fr>
2021-02-27 21:41:19 +0100

    🐛

--- myStats/index.js
+++ myStats/index.js
@@ -11,7 +11,7 @@ try {
     const suffixes = { '1': 'st', '2': 'nd', '3': 'rd' };
     userData.rank = `${rank}${suffixes[rank[rank.length - 1]] || 'th'}`;
 
-    userData.correctAnswers = data.leaderboard.filter(u => u.name === user).wins;
+    userData.correctAnswers = data.leaderboard.find(u => u.name === user).wins;
 
     core.setOutput('closeIssueMsg', `Here are your statistics:\n\n    - Correct answers: ${userData.correctAnswers}\n    - Rank: ${userData.rank}`);
 
