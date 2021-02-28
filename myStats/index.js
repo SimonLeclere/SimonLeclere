64c4e953f26d59ec2d5ae02aa5dfb4dce38ddf0e
SimonLeclere <simon-leclere@orange.fr>
2021-02-28 19:54:28 +0100

    🐛 Fix a little bug in stats

--- myStats/index.js
+++ myStats/index.js
@@ -7,13 +7,14 @@ try {
 
     const data = require('../data.json');
 
-    const rank = `${data.leaderboard.filter(u => u.wins > data.leaderboard.find(x => x.name === user).wins).length + 1}`;
+    const userLeaderboard = data.leaderboard.find(x => x.name === user);
+    if(!userLeaderboard) return core.setOutput('closeIssueMsg', `Hey, ${user}, it seems that you have never played before, so you don't have any statistics. Start by answering a question ;)`);
+
+    const rank = `${data.leaderboard.filter(u => u.wins > userLeaderboard.wins).length + 1}`;
     const suffixes = { '1': 'st', '2': 'nd', '3': 'rd' };
     userData.rank = `${rank}${suffixes[rank[rank.length - 1]] || 'th'}`;
 
-    userData.correctAnswers = data.leaderboard.find(u => u.name === user).wins;
-
-    core.setOutput('closeIssueMsg', `Here are your statistics:\n\n    - Correct answers: ${userData.correctAnswers}\n    - Rank: ${userData.rank}`);
+    core.setOutput('closeIssueMsg', `Here are your statistics:\n\n    - Correct answers: ${userLeaderboard.wins}\n    - Rank: ${userData.rank}`);
 
 } catch(error) {
     core.setFailed(error.message);
